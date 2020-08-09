fe385b800ff55dea7c1c09b0c08b68dc568595a9
sliver123! <sanjayjain00@gmail.com>
2020-08-09 12:45:00 +0530

    ts2

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -8,4 +8,24 @@ public class SomeTest extends Base {
         run();
     }
 
+    @Test public void test2() {
+        run();
+    }
+
+    @Test public void test3() {
+        run();
+    }
+
+    @Test public void test4() {
+        run();
+    }
+
+    @Test public void test5() {
+        run();
+    }
+
+    @Test public void test6() {
+        run();
+    }
+
 }
